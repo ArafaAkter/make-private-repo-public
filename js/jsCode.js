f2b0f71c1db459747e24de067624d49f5b698fc7
Arafa Akter <arafaakter924@gmail.com>
2023-08-21 19:56:49 +0600

    commit

--- js/jsCode.js
+++ js/jsCode.js
@@ -1,1 +1,72 @@
-console.log('js code found');
+function handelBtn1(){
+    
+    const textContent =document.getElementById('text1').innerText;
+    const clickedItems = document.getElementById('clicked-item');
+    const serial=clickedItems.childElementCount;
+    const p = document.createElement('p');
+    p.innerHTML=`${1+serial}. ${textContent}`;
+    clickedItems.appendChild(p);  
+}
+
+  function handelBtn2(){
+      
+      const textContent =document.getElementById('text2').innerText;
+      const clickedItems = document.getElementById('clicked-item');
+      const serial=clickedItems.childElementCount;
+      const p = document.createElement('p');
+      p.innerHTML=`${1+serial}. ${textContent}`;
+      clickedItems.appendChild(p);
+     
+  
+  }
+  
+  function handelBtn3(){
+      
+      const textContent =document.getElementById('text3').innerText;
+      const clickedItems = document.getElementById('clicked-item');
+      const serial=clickedItems.childElementCount;
+      const p = document.createElement('p');
+      p.innerHTML=`${1+serial}. ${textContent}`;
+      clickedItems.appendChild(p);
+      
+  
+  }
+  
+  function handelBtn4(){
+      
+      const textContent =document.getElementById('text4').innerText;
+      const clickedItems = document.getElementById('clicked-item');
+      const serial=clickedItems.childElementCount;
+      const p = document.createElement('p');
+      p.innerHTML=`${1+serial}. ${textContent}`;
+      clickedItems.appendChild(p);
+      
+  
+  }
+  
+  function handelBtn5(){
+      
+      const textContent =document.getElementById('text5').innerText;
+      const clickedItems = document.getElementById('clicked-item');
+      const serial=clickedItems.childElementCount;
+      const p = document.createElement('p');
+      p.innerHTML=`${1+serial}. ${textContent}`;
+      clickedItems.appendChild(p);
+     
+  
+  }
+  
+  function handelBtn6(){
+      
+      const textContent =document.getElementById('text6').innerText;
+      const clickedItems = document.getElementById('clicked-item');
+      const serial=clickedItems.childElementCount;
+      const p = document.createElement('p');
+      p.innerHTML=`${1+serial}. ${textContent}`;
+      clickedItems.appendChild(p);
+      
+  
+  }
+  
+
+
